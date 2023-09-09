2080184972d1c379acc4f39acc898400a41759c7
Tasi Pasin <tasi.pasin96@gmail.com>
2023-09-09 16:07:03 -0300

    Adicionados comentários.

--- temp-sensor/temp-sensor.ino
+++ temp-sensor/temp-sensor.ino
@@ -3,6 +3,7 @@
 
 // O fio de dados é conectado no pino digital D4 no ESP
 #define ONE_WIRE_BUS D4
+// Índice do sensor
 #define SENSOR_D4_INDEX 0
 // Prepara uma instância oneWire para comunicar com qualquer outro dispositivo oneWire
 OneWire oneWire(ONE_WIRE_BUS);
@@ -11,21 +12,33 @@ DallasTemperature sensors(&oneWire);
 
 float temperature = 0;
 
-float requestTemp(int index){
+/**
+* Realiza Setup inicial dos sensores de temperatura.
+*/
+void setupTempSensor(void){
+  sensors.begin();
+}
+
+/**
+* Retorna a temperatura, em ºC no index do sensor.
+* @param index Indica o índice do sensor.
+* @return Retorna a temperatura do sensor.
+*/
+float requestTemp(int index) {
+  // Envia comando para ler temperaturas dos sensores
   sensors.requestTemperatures();
+  // Retorna a temperatura no sensor contido no index
   return sensors.getTempCByIndex(index);
 }
 
 void setup(void) {
   Serial.begin(9600);
-  sensors.begin();  // Inicia a biblioteca
+  
 }
 void loop(void) {
   float temperatureNow = requestTemp(SENSOR_D4_INDEX);
   if (temperature != temperatureNow) {
     temperature = temperatureNow;
-    // Manda comando para ler temperaturas
-    // Escreve a temperatura em Celsius
     Serial.print("Temperatura: ");
     Serial.print(temperature);
     Serial.println(" graus");
